1803548f40ae4c19f3bca687750a469edcbc79f6
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2023-01-03 08:47:01 +0100

    Add files via upload

---
+++ php/quizz.php
@@ -0,0 +1,13 @@
+
+    
+
+<?php
+echo "<h2>Welcome to this PHP quizz challenge!</h2>";
+$name = (string) readline("Whats your name ?: ");
+echo "Hello ".$name. " this quizz got 5 question  about PHP with some within Multiple choice";
+echo "Are you ready?";
+$arr = explode('',readline());
+
+$str = "Yes,No,Maybe,No YOU ARE YOU READY!!!!???";
+echo print_r($arr)
+?>
